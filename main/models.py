e6cca863a610d6dcd9e7c8298d5ab69e084e6ebd
Earl Pearl <earlpearl17@gmail.com>
2020-05-01 06:32:09 -0400

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Todo(models.Model):
+
+	title = models.CharField(max_length=255)
+	due_date = models.DateField()
+	done = models.BooleanField(default=False)
+	description = models.TextField()
